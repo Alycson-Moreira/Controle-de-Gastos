488d03971fc2ab681760cac9e00e9715852f075b
Alycson-Moreira <alycson2@gmail.com>
2024-01-06 23:51:39 -0300

    Criado função para gerar os vencimentos automaticamente no Select atravez do uso do for, corrigido o titulo da pagina.

--- controle.js
+++ controle.js
@@ -1,17 +1,30 @@
 let meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
         'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
+//Localizar o mes atual e retornar no html associando ao mes correspondente do array.
     function retornaMes() {
         let mes = new Date();
-        let recebe_Mes = mes.getMonth() 
+        let recebe_Mes = mes.getMonth(); 
 
         for (let i = 0; i < meses.length; i++) {
             if (recebe_Mes == i) {
                 
                 let mesdoArray = document.getElementById("mesAtual");
-                return  mesdoArray.innerHTML += meses[i];
+                mesdoArray.innerHTML += meses[i];
             }
         }
-    }   
-retornaMes();    
+    }
+
+//Gerar vencimentos automaticamente no Select atravez do uso do for
+    function geraVencimentos() {
+        let select = document.querySelector("#vencimentos");
+
+        for (let i = 1; i <= 31; i++) {
+            select.options[select.options.length] = new Option(i);
+        }
+        
+
+    }        
+geraVencimentos();
+retornaMes();
 
